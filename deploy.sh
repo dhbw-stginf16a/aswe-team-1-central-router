83de8fe0b8673313b01c8ef59f2beae1b85cc25b
Thore <thore@kruess.xyz>
2019-03-19 02:28:08 +0100

    Add travis build

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build -t asweteam1/central-router:latest -t asweteam1/central-router:$TRAVIS_TAG --label version="$TRAVIS_TAG" .
+docker push asweteam1/central-router:latest
+docker push asweteam1/central-router:$TRAVIS_TAG
