600874495ca6d0e687c083f9c9c4f842b72ea2ba
Dorian Czichotzki <dorian.czichotzki@de.ibm.com>
2019-03-20 10:57:20 +0100

    Deployment Fix

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+tar -cf deploy.tar Pipfile Pipfile.lock openapi app.py api
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t asweteam1/central-router:latest -t asweteam1/central-router:$TRAVIS_TAG --label version="$TRAVIS_TAG" .
 docker push asweteam1/central-router:latest
