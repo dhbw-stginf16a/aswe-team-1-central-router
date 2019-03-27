022249424a6a5c4ec869d132f3a0d299b071205d
Thore <thore@kruess.xyz>
2019-03-28 00:34:14 +0100

    Send message to chat adapter

--- api/skill.py
+++ api/skill.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
 import logging
+import os
+
+import requests
 
 from api.models.Skill import Skill, SKILLS
 
 logger = logging.getLogger(__name__)
+
+CHAT_ADAPTER_URL = os.environ.setdefault('CHAT_ADAPTER_URL', 'localhost:8082/api/v1')
 
 
 def registerSkill(body):
@@ -16,4 +21,9 @@ def getSkills():
     return list(SKILLS.keys()), 200
 
 def proactive(body):
-    pass
+    try:
+        requests.post(f'{CHAT_ADAPTER_URL}/proactive', json=body)
+    except requests.ConnectionError:
+        logger.error('Timeout on connection to chat adapter')
+    finally:
+        return '', 204
