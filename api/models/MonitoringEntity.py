85630e0dd78f4066a2a0e93a222df4660484dc94
Erik Z <guserav@users.noreply.github.com>
2019-04-07 13:07:46 +0200

    Hand through http status codes to skill

--- api/models/MonitoringEntity.py
+++ api/models/MonitoringEntity.py
@@ -22,7 +22,7 @@ class MonitoringEntity:
         # assert r.status_code == 200
         json_resp = r.json()
         assert type(json_resp) == list
-        return json_resp
+        return json_resp, r.status_code
 
     def requestIfConcern(self, concern, rtype, payload):
         if self.concern == concern:
@@ -41,13 +41,14 @@ class MonitoringEntityManager:
 
     def requestConcern(self, concern, rtype, payload):
         responses = []
-
+        return_code = 200
         for monent in self.monents:
-            resps = monent.requestIfConcern(concern, rtype, payload)
+            resps, status_code = monent.requestIfConcern(concern, rtype, payload)
+            return_code = max(return_code, status_code)
             for resp in resps:
                 responses.append(resp)
 
-        return responses
+        return responses, return_code
 
     def getConcerns(self):
         return [monent.getConcern() for monent in self.monents]
